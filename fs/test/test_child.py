0f097175f4b8f37cbb949158cd7f4b1301c93bc8
Tobias Brox <tobixen@gmail.com>
2008-07-10 14:06:32 +0200

    Testing some few more corner cases

--- fs/test/test_child.py
+++ fs/test/test_child.py
@@ -23,9 +23,17 @@ def test_bad_slash():
         'child name contains directory separator',
         )
 
+    ## Exception should be raised even if it's not evil (?)
+    e = assert_raises(fs.InsecurePathError, fs.root.child, u'notsoevil/')
+
 def test_bad_dotdot():
     e = assert_raises(fs.InsecurePathError, fs.root.child, u'..')
     eq(
         str(e),
         'child trying to climb out of directory',
         )
+
+    ## of course, those should also raise errors
+    assert_raises(fs.InsecurePathError, fs.root.child, u'../')
+    assert_raises(fs.InsecurePathError, fs.root.child, u'..//')
+    assert_raises(fs.InsecurePathError, fs.root.child, u'..//..')
