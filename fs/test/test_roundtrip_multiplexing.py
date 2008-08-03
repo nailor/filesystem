dfe2702acca1d75c09dd6a4bf8a1cda4748487d2
Tobias Brox <tobixen@gmail.com>
2008-08-03 15:31:57 +0400

    oups.  Lots of broken tests.

--- fs/test/test_roundtrip_multiplexing.py
+++ fs/test/test_roundtrip_multiplexing.py
@@ -1,10 +1,22 @@
+from fs.test.util import (
+    maketemp,
+    )
 from fs.test import test_roundtrip
 
 import fs.multiplexing
+import fs
 
 class MultiPlexing_Tests(test_roundtrip.OperationsMixin):
     def setUp(self):
         self.path = fs.multiplexing.path()
         assert not self.path.exists()
         self.path.mkdir(create_parents=True, may_exist=True)
+        assert self.path.exists()
+
+
+class MultiPlexingLocalFS_Tests(test_roundtrip.OperationsMixin):
+    def setUp(self):
+        real_path = fs.path(maketemp())
+        self.path = fs.multiplexing.path()
+        self.path.bind(real_path)
         assert self.path.exists()
