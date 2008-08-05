b0376458eaa0525c70577353d0cce62c64056382
Tobias Brox <tobixen@gmail.com>
2008-08-05 11:49:51 +0400

    more tests pass - uncovered a bug with multiplexing fs (__iter__ wouldn't check the bound path)

---
+++ fs/test/test_copyonwrite_iter.py
@@ -0,0 +1,36 @@
+from __future__ import with_statement
+import tempfile
+import os
+
+from nose.tools import eq_ as eq
+
+from fs.test.util import assert_raises, maketemp
+    
+import fs
+import fs.copyonwrite
+
+
+def test_iter():
+    temp_dir = maketemp()
+    temp_files = ['file1', 'file2']
+    # put some files in the temporary directory
+    for i in temp_files:
+        f = open(os.path.join(temp_dir, i), 'w')
+        f.close()
+    p = fs.copyonwrite.path(fs.path(temp_dir))
+    ## add one more file
+    with p.child('file3').open('w') as f:
+        f.write('ubba')
+    temp_files.append('file3')
+    # see whether we actually get the file names with the iterator
+    eq(sorted(str(x) for x in p), sorted(str(p.child(x)) for x in temp_files))
+
+def test_not_directory():
+    temp_dir = maketemp()
+    # prepare a file on which to call the iterator
+    f = open(os.path.join(temp_dir, "some_file"), "w")
+    f.close()
+    # check reaction on getting the iterator
+    p = fs.copyonwrite.path(fs.path(temp_dir).join("some_file"))
+    # note: the exception is only raised after calling ``next``
+    assert_raises(OSError, list, p)
