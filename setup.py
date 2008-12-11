99ecb9dab363ef707e712b126b03e930b1e39c23
Tommi Virtanen <tv@eagain.net>
2008-12-10 18:02:25 -0800

    Add a placeholder setup.py, I need an egg of this.

---
+++ setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+from setuptools import setup, find_packages
+import os
+
+setup(
+    name = "fs",
+    version = "0.1",
+    packages = find_packages(),
+
+    author = "TODO",
+    author_email = "TODO",
+    description = "Pythonic filesystem API",
+    long_description = """
+
+TODO
+
+""".strip(),
+    license = "MIT",
+    keywords = "filesystem",
+#    url = "http://eagain.net/software/fs/",
+
+    )
+
