88ee65cc24079b74d4dcf9c1273e72204b4a50c3
Tommi Virtanen <tv@eagain.net>
2009-07-02 09:00:35 +0100

    Fill out setup.py for PyPI, prepare to release 0.2

--- setup.py
+++ setup.py
@@ -4,20 +4,41 @@ import os
 
 setup(
     name = "filesystem",
-    version = "0.1",
+    version = "0.2",
     packages = find_packages(),
 
-    author = "TODO",
-    author_email = "TODO",
+    author = "Tommi Virtanen",
+    author_email = "tv@eagain.net",
+    # and others; thank you all
+
     description = "Pythonic filesystem API",
     long_description = """
 
-TODO
+A pythonic and flexible filesystem API.
+
+This package implements accessing the local
+filesystem, as implemented by your operating
+system, and an inmemory filesystem.
+
+Other packages are expected to provide the
+same API, for accessing various other things
+as files.
 
 """.strip(),
     license = "MIT",
     keywords = "filesystem",
 #    url = "http://eagain.net/software/fs/",
+    url = "http://eagain.net/gitweb/?p=fs.git",
 
+    classifiers = """\
+Intended Audience :: Developers
+License :: OSI Approved :: MIT License
+Programming Language :: Python
+Topic :: Software Development :: Libraries
+Topic :: System :: Filesystems
+Development Status :: 3 - Alpha
+""".splitlines(),
+# TODO Development Status :: 4 - Beta
+# TODO Development Status :: 5 - Production/Stable
     )
 
